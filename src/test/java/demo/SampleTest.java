b7d054fc02ce856d895c37a21e7a70a7cf087593
Kalbande <bhushan.kalbande@lenze.com>
2020-06-24 13:33:15 +0530

    updated sample test

--- src/test/java/demo/SampleTest.java
+++ src/test/java/demo/SampleTest.java
@@ -22,7 +22,7 @@ class SampleTest {
         String chromedriverpath=directoryPath + "\\src\\main\\resources\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", chromedriverpath);
         String url="http://apps.mosaiq.one";
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.get(url);
         driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, 120);
